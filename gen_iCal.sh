658ecaa2ccf26378704a7d307382c02d3a4f5de6
Kevin Konrad <keko0003@stud.hs-kl.de>
2017-12-18 19:54:27 +0100

    Korrektur Schichtbeginn 2018

--- gen_iCal.sh
+++ gen_iCal.sh
@@ -1,5 +1,5 @@
 pdflatex gen_iCal.tex | \
 tail -n +16 > ./Schichtplan.ics && \
-head -n $(( $(wc -l ./Schichtplan.ics | awk '{print $1}') - 7 )) ./Schichtplan.ics | \
+head -n $(( $(wc -l ./Schichtplan.ics | awk '{print $1}') - 6 )) ./Schichtplan.ics | \
 tee ./Schichtplan.ics > /dev/null && \
 rm gen_iCal.{aux,log}

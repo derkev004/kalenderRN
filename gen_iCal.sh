1f60bf54d08e5657259a43a670974fb4f1acb5f7
Kevin Konrad <kevinkonrad@ymail.com>
2017-12-19 02:33:55 +0100

    seltsamen bug gefixt

--- gen_iCal.sh
+++ gen_iCal.sh
@@ -1,5 +1,5 @@
 pdflatex gen_iCal.tex | \
-tail -n +16 > ./Schichtplan.ics && \
+tail -n +16 > ./Schichtplan.ics;
 head -n $(( $(wc -l ./Schichtplan.ics | awk '{print $1}') - 6 )) ./Schichtplan.ics | \
 sed 's/$'"/`echo \\\r`/" > ./Schichtplan.ics && \
 rm gen_iCal.{aux,log}

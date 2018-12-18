e34b6b8cd51354ce9500752ddfad395cdbabf367
Kevin Konrad <kevinkonrad@ymail.com>
2018-12-18 22:27:15 +0100

    Schichtplan für 2019 aktualisieren, iCal-Erzeugung vereinfachen

---
+++ Schichtplan_E_2019.sh
@@ -0,0 +1,3 @@
+pdflatex Schichtplan_E_2019.tex
+gsed -i 's/$/\r/' Schichtplan_E_2019.ics
+rm Schichtplan_E_2019.{aux,log}
--- gen_iCal.sh
+++
@@ -1,5 +0,0 @@
-pdflatex gen_iCal.tex | \
-tail -n +16 > ./Schichtplan.ics;
-head -n $(( $(wc -l ./Schichtplan.ics | awk '{print $1}') - 6 )) ./Schichtplan.ics | \
-sed 's/$'"/`echo \\\r`/" > ./Schichtplan.ics && \
-rm gen_iCal.{aux,log}
